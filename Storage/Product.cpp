37ba97de2fb49a1f6ec7f7118825c126bb48a79d
icachev <ivaylo_66@abv.bg>
2017-01-02 01:33:43 +0200

    init header and cpp file

--- Storage/Product.cpp
+++ Storage/Product.cpp
@@ -1,9 +1,27 @@
 #include "Product.h"
 
 Product::Product(){
-
+	product_name = "";
+	exp_date = ""; // dd/mm/yyyy format
+	entry_date = ""; // dd/mm/yyyy format
+	producer_name = "";
+	unit = "";
+	quantity = 0.0;
+	in_storage_place = "" //sektsiq.raft.nomer format
+	comment = "";
 }
 
-Product::Product(const Product& other){
-
+void Product::input() {
+	cout << "Enter product's name: ";
+	cin >> producer_name;
+	cout << "Enter expiration date (dd/mm/yyyy format): ";
+	cin >> exp_date;
+	cout << "Enter date of product's entry (dd/mm/yyyy format): ";
+	cin >> entry_date;
+	cout << "Enter producer's name: ";
+	cin >> producer_name;
+	cout << "Enter quantity and units of the product(e.g. 10 kilograma): ";
+	cin >> quantity >> unit;
+	cout << "Enter in storage place of the product (section.shelf.number format): ";
+	cin >> in_storage_place;
 }
--- Storage/Product.h
+++ Storage/Product.h
@@ -1,13 +1,27 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
 #ifndef PRODUCT_H
 #define PRODUCT_H
 
 
 class Product
 {
-    public:
-        Product();
-        Product(const Product& other);
-    private:
+private:
+	string product_name;
+	string exp_date; // dd/mm/yyyy format
+	string entry_date; // dd/mm/yyyy format
+	string producer_name;
+	string unit;
+	double quantity;
+	string in_storage_place //sektsiq.raft.nomer format
+	string comment;
+public:
+	Product();
+	void input();
+	void output() const;
+
 };
 
 #endif
